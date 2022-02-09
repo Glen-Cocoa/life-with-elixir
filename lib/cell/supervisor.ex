125dfbc41ef7e93c643e09a8c7abf813cadcaa92
eli.steiger <eli.steiger@covermymeds.com>
2022-02-09 16:36:06 -0500

    WIP

--- lib/cell/supervisor.ex
+++ lib/cell/supervisor.ex
@@ -1,29 +1,43 @@
 defmodule Cell.Supervisor do
-  use Supervisor
+  use DynamicSupervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def start_child(module, position) do
+    child_spec = %{
+      id: module,
+      start: {module, :start_link, position}
+    }
+    DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
 
   def init([]) do
     children = [
-      # TODO: Update deprecated method
-      worker(Cell, [])
+    # TODO: Update deprecated method
+      # worker(Cell, [])
+      %{
+        id: Cell,
+        start: {Cell, :start_link}
+      }
     ]
     # simple_one_for_one strategy means
     # children will be added & removed to the supervision tree dynamically
     # children will be Cell worker processes (deprecated?)
     # :transient means when Cell process terminates with :normal or :shutdown it will not be restarted
     # Supervisor will restart a process dying with any other message
+
       # TODO: Update deprecated method
-    supervise(children, strategy: :simple_one_for_one, restart: :transient)
+    # supervise(children, strategy: :simple_one_for_one, restart: :transient)
+    DynamicSupervisor.start_link(children, strategy: :simple_one_for_one)
     # Supervisor.start_link(children, strategy: :simple_one_for_one, restart: :transient)
   end
 
   # return enum of pids of all children being supervised
   def children do
     Cell.Supervisor
-    |> Supervisor.which_children
+    |> DynamicSupervisor.which_children
     |> Enum.map(fn {_, pid, _, _} -> pid end)
   end
 
--- lib/universe/supervisor.ex
+++ lib/universe/supervisor.ex
@@ -1,7 +1,7 @@
 defmodule Universe.Supervisor do
   use DynamicSupervisor
 
-  def start(_type, _args) do
+  def start do
     start_link()
   end
 
@@ -9,8 +9,8 @@ defmodule Universe.Supervisor do
     DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def start_child() do
-    children = [
+  def start_child do
+    child_spec = [
       %{
         id: Universe,
         start: {Universe, :start_link, []}
@@ -27,28 +27,10 @@ defmodule Universe.Supervisor do
       }
     ]
 
-    DynamicSupervisor.start_child(__MODULE__, children)
+    DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
 
   def init(_) do
-    # specifies Universe as worker module and calls Universe.start_link with arg "[]"
-    children = [
-      %{
-        id: Universe,
-        start: {Universe, :start_link, []}
-      },
-      %{
-        id: Cell.Supervisor,
-        start: {Cell.Supervisor, :start_link, []},
-        type: :supervisor
-      },
-      %{
-        id: Registry,
-        start: {Registry, :start_link, []},
-        type: :supervisor
-      }
-    ]
-
     # :one_for_one strategy means that children will be started immediately
     # upon failure, the failed child will be restarted
     DynamicSupervisor.init(strategy: :one_for_one)
