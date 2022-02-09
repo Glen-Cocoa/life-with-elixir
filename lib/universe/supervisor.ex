32f976a8d68caf5f8c71e69a6e518537dd3321d2
eli.steiger <eli.steiger@covermymeds.com>
2022-02-09 15:45:31 -0500

    update Universe Supervisor to use DynamicSupervisor

--- lib/universe/supervisor.ex
+++ lib/universe/supervisor.ex
@@ -1,31 +1,57 @@
 defmodule Universe.Supervisor do
-  use Supervisor
+  use DynamicSupervisor
 
   def start(_type, _args) do
     start_link()
   end
 
   def start_link do
-    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def start_child() do
+    children = [
+      %{
+        id: Universe,
+        start: {Universe, :start_link, []}
+      },
+      %{
+        id: Cell.Supervisor,
+        start: {Cell.Supervisor, :start_link, []},
+        type: :supervisor
+      },
+      %{
+        id: Registry,
+        start: {Registry, :start_link, []},
+        type: :supervisor
+      }
+    ]
+
+    DynamicSupervisor.start_child(__MODULE__, children)
   end
 
   def init(_) do
+    # specifies Universe as worker module and calls Universe.start_link with arg "[]"
     children = [
-      # specifies Universe as worker module and calls Universe.start_link with arg "[]"
-        # TODO: Update deprecated method
-      worker(Universe, []),
-      # specifies Cell.Supervisor as sv module and calls Cell.Supervisor.start_link with arg "[]"
-
-        # TODO: Update deprecated method
-      supervisor(Cell.Supervisor, []),
-        # TODO: Update deprecated method
-      supervisor(Registry, [:unique, Cell.Registry])
+      %{
+        id: Universe,
+        start: {Universe, :start_link, []}
+      },
+      %{
+        id: Cell.Supervisor,
+        start: {Cell.Supervisor, :start_link, []},
+        type: :supervisor
+      },
+      %{
+        id: Registry,
+        start: {Registry, :start_link, []},
+        type: :supervisor
+      }
     ]
 
     # :one_for_one strategy means that children will be started immediately
     # upon failure, the failed child will be restarted
-      # TODO: Update deprecated method
-    supervise(children, strategy: :one_for_one)
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 
 end
